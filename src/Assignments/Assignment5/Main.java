10977810b70f28f0b7e4b700824f0e5bbe601a14
RajDhende <dhenderaj2003@gmail.com>
2023-11-01 22:45:32 +0530

    .

--- src/Assignments/Assignment5/Main.java
+++ src/Assignments/Assignment5/Main.java
@@ -1,93 +1,83 @@
 package Assignments.Assignment5;
-import java.util.Scanner;
 import java.util.Iterator;
+import java.util.Scanner;
 
-/**
- * A simple interactive program for manipulating a LinkedPositionalList of Integers.
- */
 public class Main {
-    /**
-     * Main entry point for the program.
-     *
-     * @param args The command-Int arguments (not used in this program).
-     */
     public static void main(String[] args) {
-        LinkedPositionalList<Integer> list = new LinkedPositionalList<>();
+        LinkedPositionalList<String> list = new LinkedPositionalList<>();
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            System.out.println("\nMenu:");
-            System.out.println("1. Add Element at the Front");
-            System.out.println("2. Add Element at the Back");
-            System.out.println("3. Add Element Before a Position");
-            System.out.println("4. Add Element After a Position");
-            System.out.println("5. Replace Element at a Position");
-            System.out.println("6. Remove Element at a Position");
-            System.out.println("7. Print List");
-            System.out.println("8. Exit");
-            System.out.print("Enter your choice: ");
+            System.out.println("LinkedPositionalList Menu:");
+            System.out.println("1. Add element at the front");
+            System.out.println("2. Add element at the back");
+            System.out.println("3. Add element before a position");
+            System.out.println("4. Add element after a position");
+            System.out.println("5. Remove element at a position");
+            System.out.println("6. Print the list");
+            System.out.println("7. Exit");
 
+            System.out.print("Enter your choice: ");
             int choice = scanner.nextInt();
 
             switch (choice) {
                 case 1:
-                    System.out.print("Enter the element to add at the front: ");
-                    Integer element = scanner.nextInt();
+                    System.out.print("Enter element to add at the front: ");
+                    String element = scanner.next();
                     list.addFirst(element);
                     break;
+
                 case 2:
-                    System.out.print("Enter the element to add at the back: ");
-                    element = scanner.nextInt();
+                    System.out.print("Enter element to add at the back: ");
+                    element = scanner.next();
                     list.addLast(element);
                     break;
+
                 case 3:
-                    System.out.print("Enter the element to add: ");
-                    element = scanner.nextInt();
+                    System.out.print("Enter element to add: ");
+                    element = scanner.next();
                     System.out.print("Enter the position before which to add: ");
-                    Position<Integer> beforePos = list.first();
-                    list.addBefore(beforePos, element);
+                    int positionBefore = scanner.nextInt();
+                    list.addBefore(list.positions().iterator().next(), element);
                     break;
+
                 case 4:
-                    System.out.print("Enter the element to add: ");
-                    element = scanner.nextInt();
+                    System.out.print("Enter element to add: ");
+                    element = scanner.next();
                     System.out.print("Enter the position after which to add: ");
-                    Position<Integer> afterPos = list.first();
-                    list.addAfter(afterPos, element);
+                    int positionAfter = scanner.nextInt();
+                    list.addAfter(list.positions().iterator().next(), element);
                     break;
+
                 case 5:
-                    System.out.print("Enter the new element: ");
-                    element = scanner.nextInt();
-                    System.out.print("Enter the position to replace: ");
-                    Position<Integer> replacePos = list.first();
-                    list.set(replacePos, element);
-                    break;
-                case 6:
                     System.out.print("Enter the position to remove: ");
-                    Position<Integer> removePos = list.first();
-                    list.remove(removePos);
+                    int positionToRemove = scanner.nextInt();
+                    list.remove(list.positions().iterator().next());
                     break;
-                case 7:
+
+                case 6:
+                    System.out.println("LinkedPositionalList:");
                     printList(list);
                     break;
-                case 8:
-                    System.out.println("Exiting the program.");
+
+                case 7:
+                    System.out.println("Exiting program.");
+                    scanner.close();
                     System.exit(0);
+                    break;
+
                 default:
                     System.out.println("Invalid choice. Please try again.");
             }
         }
     }
 
-    /**
-     * Prints the contents of the given LinkedPositionalList.
-     *
-     * @param list The LinkedPositionalList to print.
-     */
-    private static void printList(LinkedPositionalList<Integer> list) {
-        Iterator<Integer> iterator = list.iterator();
-        System.out.println("List Contents:");
+    public static void printList(LinkedPositionalList<String> list) {
+        Iterator<String> iterator = list.iterator();
         while (iterator.hasNext()) {
-            System.out.print(iterator.next() + " ");
+            System.out.print(iterator.next());
         }
+        System.out.println();
     }
+
 }
