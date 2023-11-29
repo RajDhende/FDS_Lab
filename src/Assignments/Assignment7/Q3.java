b0221dad3e45af22d45014368914f26e3179d469
RajDhende <dhenderaj2003@gmail.com>
2023-11-29 23:47:45 +0530

    .

--- src/Assignments/Assignment7/Q3.java
+++ src/Assignments/Assignment7/Q3.java
@@ -33,8 +33,25 @@ class ExpressionTree {
         TreeNode root = new TreeNode("-");
         root.left = new TreeNode("/");
         root.right = new TreeNode("+");
+        root.left.left = new TreeNode("*");
+        root.left.right = new TreeNode("+");
 
-        // ... (continuing tree construction)
+        root.left.left.left = new TreeNode("3");
+        root.left.left.right = new TreeNode("1");
+
+        root.left.right.left = new TreeNode("-");
+        root.left.right.right = new TreeNode("2");
+
+        root.left.right.left.left = new TreeNode("9");
+        root.left.right.left.right = new TreeNode("5");
+
+        root.right.left = new TreeNode("*");
+        root.right.right = new TreeNode("6");
+
+        root.right.left.left = new TreeNode("3");
+        root.right.left.right = new TreeNode("-");
+        root.right.left.right.left = new TreeNode("7");
+        root.right.left.right.right = new TreeNode("4");
 
         return root;
     }
